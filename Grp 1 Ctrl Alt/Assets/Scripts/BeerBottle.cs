51875fd6ed71a662b195839e7a91300097c76ac9
Bard Avelia <a.bard@rubika-edu.com>
2025-09-12 14:36:46 +0200

    fix beer bottle and player collision

--- Grp 1 Ctrl Alt/Assets/Scripts/BeerBottle.cs
+++ Grp 1 Ctrl Alt/Assets/Scripts/BeerBottle.cs
@@ -5,6 +5,8 @@ public class BeerBottle : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        print("boop");
+
         if (other.CompareTag("Player"))
         {
             GameManager.INSTANCE.BeerRessourceReference.Add(1);
