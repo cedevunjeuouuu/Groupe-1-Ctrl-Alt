84e8f969e2c79638510b3967ccd6c9734886b8aa
Lu k <Lucas.kominiarz@laposte.net>
2025-09-15 10:54:06 +0200

    Auto stash before merge of "RandomHouses" and "origin/RandomHouses"

--- Grp 1 Ctrl Alt/Assets/Scripts/WorldGeneration.cs
+++ Grp 1 Ctrl Alt/Assets/Scripts/WorldGeneration.cs
@@ -5,10 +5,8 @@ using Random = UnityEngine.Random;
 
 public class WorldGeneration : MonoBehaviour
 {
-    [SerializeField] private GameObject[] easyLevels;
-    [SerializeField] private GameObject[] mediumLevels;
-    [SerializeField] private GameObject[] hardLevels;
-    [SerializeField] private int numberOfMediumLevels = 10;
+    
+    [SerializeField] private GameObject[] levels;
     [SerializeField] private float levelLenght;
 
     private void Start()
@@ -18,20 +16,10 @@ public class WorldGeneration : MonoBehaviour
 
     IEnumerator SpawnLevel()
     {
-        for (int i = 0; i < easyLevels.Length; i++)
-        {
-            Instantiate(easyLevels[i], transform);
-            yield return new WaitForSeconds(levelLenght);
-        }
-        for (int i = 0; i < numberOfMediumLevels; i++)
-        {
-            Instantiate(mediumLevels[Random.Range(0,mediumLevels.Length)], transform);
-            yield return new WaitForSeconds(levelLenght);
-        }
-
+        int tabLenght = levels.Length;
         while (true)
         {
-            Instantiate(hardLevels[Random.Range(0,hardLevels.Length)], transform);
+            Instantiate(levels[Random.Range(0,tabLenght)], transform);
             yield return new WaitForSeconds(levelLenght);
         }
         
