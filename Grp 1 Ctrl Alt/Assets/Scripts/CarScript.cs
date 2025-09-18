6b04364f121907e31d72e1f2c187ebb4cdbe79c3
Lu k <Lucas.kominiarz@laposte.net>
2025-09-18 12:55:35 +0200

    Plus de bugs

--- Grp 1 Ctrl Alt/Assets/Scripts/CarScript.cs
+++ Grp 1 Ctrl Alt/Assets/Scripts/CarScript.cs
@@ -10,13 +10,18 @@ public class CarScript : MonoBehaviour
 
     [SerializeField]
     private bool wantToDestroy = false;
+    private bool isFirstTime = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             GameManager.INSTANCE.camera.Shake();
-            FindFirstObjectByType<SusRessource>()?.Add(penality);
+            if (isFirstTime)
+            {
+                FindFirstObjectByType<SusRessource>()?.Add(penality);
+                isFirstTime = false;
+            }
             if (wantToDestroy)
             {
                 Destroy(gameObject);
