52924b0dcc34ba868e21b08b2f133f99ebc5ee7b
Zoé <z.henriot@rubika-edu.com>
2025-09-19 11:51:17 +0200

    bug fix IMPORTANT

--- Grp 1 Ctrl Alt/Assets/Scripts/BeerBottle.cs
+++ Grp 1 Ctrl Alt/Assets/Scripts/BeerBottle.cs
@@ -1,8 +1,36 @@
+using DG.Tweening;
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class BeerBottle : MonoBehaviour
 {
+    [SerializeField] private float animspeed = 2f;
+    [SerializeField] private float animForce = 10f;
+    private bool isUp;
+    private void Start()
+    {
+        StartCoroutine(Anim());
+    }
+
+    IEnumerator Anim()
+    {
+        while (true)
+        {
+            if (isUp)
+            {
+                transform.DOMoveY(transform.position.y - animForce, 1f / animspeed);
+            }
+            else
+            {
+                transform.DOMoveY(transform.position.y + animForce, 1f / animspeed);
+            }
+            isUp = !isUp;
+            yield return new WaitForSeconds(1f / animspeed);
+            
+        }
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -11,4 +39,5 @@ public class BeerBottle : MonoBehaviour
             Destroy(gameObject);
         }
     }
+  
 }
